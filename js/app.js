22717d69a31ca05c6b2ec4859deaa3bdf1abe416
Saggitarie <saggitarie@gmail.com>
2019-10-26 01:00:12 +0900

    Implement function for reset button. #20
    ・When the reset button is clicked, all the text inside 'Type something' input and sample text in all the font cards are replaced with an empty string.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
 //Define variables to control input values.
-var inputValue;
+var inputValue, resetButton;
 
 
 // Event Listener for 'Type Something' Input Element
 inputValue = document.querySelector('.major-navigation__text-input');
+resetButton = document.querySelector('.major-navigation__reset');
 
 inputValue.addEventListener('input', function() {
   var customTextByUser = inputValue.value;
 
   var sampleTexts = document.getElementsByClassName('card__sample-text');
-  console.log(sampleTexts);
 
   for(var sampleText of sampleTexts) {
     sampleText.textContent = customTextByUser;
+  }
+});
+
+//Reset Button
+resetButton.addEventListener('click', function() {
+  inputValue.value = '';
+
+  var sampleTexts = document.getElementsByClassName('card__sample-text');
+
+  for(var sampleText of sampleTexts) {
+    sampleText.textContent = '';
   }
 });
