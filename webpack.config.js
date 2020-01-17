fd6c41a2f18ec7109aea71956e56deac7cc7fa90
Saggitarie <saggitarie@gmail.com>
2020-01-17 18:47:49 +0900

    Webpack configuration accomplished with CSS!!
    I installed npm-run-all to run multiple npm scripts (one for compiling css and other for loading assets with webpack)
    
    Works fine so far!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const isDevelopment = process.env.NODE_ENV === "development";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -14,6 +16,10 @@ module.exports = {
     new HTMLWebpackPlugin({
       filename: "index.html",
       template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
     })
   ],
   module: {
@@ -24,7 +30,38 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          { loader: "sass-loader", options: { sourceMap: isDevelopment } }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
       }
     ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"]
   }
 };
